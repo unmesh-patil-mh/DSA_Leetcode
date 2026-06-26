a964d2d11aa8f85d3979445ee3173d208520ae26
Unmesh Patil <punmesh56@gmail.com>
2026-06-26 20:24:52 +0530

    Time: 44 ms (37.71%), Space: 92.6 MB (39.34%) - LeetHub

---
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.java
@@ -0,0 +1,61 @@
+class Solution {
+
+    static class Fenwick {
+        int[] bit;
+
+        Fenwick(int n) {
+            bit = new int[n + 1];
+        }
+
+        void update(int idx) {
+            while (idx < bit.length) {
+                bit[idx]++;
+                idx += idx & -idx;
+            }
+        }
+
+        long query(int idx) {
+            long sum = 0;
+            while (idx > 0) {
+                sum += bit[idx];
+                idx -= idx & -idx;
+            }
+            return sum;
+        }
+    }
+
+    public long countMajoritySubarrays(int[] nums, int target) {
+
+        int n = nums.length;
+
+        int[] pref = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            pref[i + 1] = pref[i] + (nums[i] == target ? 1 : -1);
+        }
+
+        int[] values = pref.clone();
+        java.util.Arrays.sort(values);
+
+        int m = 0;
+        for (int x : values) {
+            if (m == 0 || values[m - 1] != x) {
+                values[m++] = x;
+            }
+        }
+
+        Fenwick ft = new Fenwick(m);
+
+        long ans = 0;
+
+        for (int x : pref) {
+
+            int idx = java.util.Arrays.binarySearch(values, 0, m, x) + 1;
+
+            ans += ft.query(idx - 1);
+
+            ft.update(idx);
+        }
+
+        return ans;
+    }
+}

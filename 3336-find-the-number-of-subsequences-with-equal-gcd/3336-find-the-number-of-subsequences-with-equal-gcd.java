0cf410928a37ea2cff1f1a98b23c2981caa31ea3
Unmesh Patil <punmesh56@gmail.com>
2026-07-14 16:00:50 +0530

    Time: 743 ms (13.89%), Space: 93.2 MB (47.22%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.java
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.java
@@ -1,50 +1,65 @@
 class Solution {
-    long mod = 1_000_000_007; 
+
+    static final int MAX = 200;
+    static final int MOD = 1_000_000_007;
+
     int gcd(int a, int b) {
-        if(b == 0) return a; 
-        return gcd(b, a % b); 
+        while (b != 0) {
+            int t = a % b;
+            a = b;
+            b = t;
+        }
+        return a;
     }
+
     public int subsequencePairCount(int[] nums) {
-        // need a dp 
-        long[][] dp = new long[202][202];
-        int n = nums.length; 
-        dp[0][0] = 1; // base case 
 
-        int[][] gcdDP = new int[201][201]; 
-        for(int i = 0; i <= 200; i++) {
-            for(int j = 0; j <= 200; j++) gcdDP[i][j] = gcd(i, j); 
+        int[][] gcdDP = new int[MAX + 1][MAX + 1];
+        for (int i = 0; i <= MAX; i++) {
+            for (int j = 0; j <= MAX; j++) {
+                gcdDP[i][j] = gcd(i, j);
+            }
         }
 
-        for(int i = 0; i < n; i++) {
-            // g1 seq 
-            long[][] nextDP = new long[201][201]; 
-            for(int g1 = 0; g1 <= 200; g1++) {
-                // g2 seq 
-                for(int g2 = 0; g2 <= 200; g2++) {
-                    long cur = dp[g1][g2]; 
-                    // skip 
-                    nextDP[g1][g2] = (nextDP[g1][g2] + cur) % mod; 
-                    // add to g1 
-                    int new_g1 = gcdDP[g1][nums[i]];
-                    nextDP[new_g1][g2] = (nextDP[new_g1][g2] + cur) % mod; 
-                    // add to g2 
-                    int new_g2 = gcdDP[g2][nums[i]]; 
-                    nextDP[g1][new_g2] = (nextDP[g1][new_g2] + cur) % mod; 
+        long[][] dp = new long[MAX + 1][MAX + 1];
+        dp[0][0] = 1;
+
+        for (int num : nums) {
+
+            long[][] next = new long[MAX + 1][MAX + 1];
+
+            for (int g1 = 0; g1 <= MAX; g1++) {
+
+                long[] dpRow = dp[g1];
+                long[] nextRow = next[g1];
+
+                for (int g2 = 0; g2 <= MAX; g2++) {
+
+                    long cur = dpRow[g2];
+                    if (cur == 0) continue;
+
+                    // Skip
+                    nextRow[g2] = (nextRow[g2] + cur) % MOD;
+
+                    // Put in first subsequence
+                    int ng1 = gcdDP[g1][num];
+                    next[ng1][g2] = (next[ng1][g2] + cur) % MOD;
+
+                    // Put in second subsequence
+                    int ng2 = gcdDP[g2][num];
+                    nextRow[ng2] = (nextRow[ng2] + cur) % MOD;
                 }
             }
 
-            for(int x = 0; x < 201; x++) {
-                for(int j = 0; j < 201; j++) {
-                    dp[x][j] = nextDP[x][j]; 
-                }
-            }
-        } 
-
-        // compute ans for last index, & g, g 
-        long ans = 0; 
-        for(int g = 1; g <= 200; g++) {
-            ans = (ans + dp[g][g]) % mod; 
+            dp = next; // Swap instead of copying
         }
-        return (int) ans; 
+
+        long ans = 0;
+        for (int g = 1; g <= MAX; g++) {
+            ans += dp[g][g];
+            if (ans >= MOD) ans -= MOD;
+        }
+
+        return (int) ans;
     }
 }

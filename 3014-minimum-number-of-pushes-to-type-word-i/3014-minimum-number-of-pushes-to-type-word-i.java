396f48b7bdd059cd14379af14eb5b2310ff88e12
Unmesh Patil <punmesh56@gmail.com>
2026-07-30 21:44:40 +0530

    Time: 0 ms (100%), Space: 43.3 MB (47.13%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int q = word.length() >> 3;
+        int r = word.length() & 7;
+        return ((q << 2) + r) * (q + 1); 
+    }
+}

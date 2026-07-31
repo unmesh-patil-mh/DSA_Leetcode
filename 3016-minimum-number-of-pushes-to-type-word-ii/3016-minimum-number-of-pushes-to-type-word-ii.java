de356b06784a01e3b2f2a4eff7e36fca7e48f136
Unmesh Patil <punmesh56@gmail.com>
2026-07-31 09:48:37 +0530

    Time: 9 ms (94.22%), Space: 48.1 MB (32.08%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,14 +1,27 @@
 class Solution {
     public int minimumPushes(String word) {
         int[] freq = new int[26];
-        for (char c : word.toCharArray()) 
+
+        for (char c : word.toCharArray()) {
             freq[c - 'a']++;
-        
+        }
+
         Arrays.sort(freq);
+
+        int pushes = 1;
+        int count = 0;
         int ans = 0;
-        for (int i = 25; i >= 0 && freq[i] > 0; i--) 
-            ans += freq[i] * ((25 - i) / 8 + 1);
-        
+
+        for (int i = 25; i >= 0 && freq[i] > 0; i--) {
+            ans += freq[i] * pushes;
+            count++;
+
+            if (count == 8) {
+                pushes++;
+                count = 0;
+            }
+        }
+
         return ans;
     }
 }

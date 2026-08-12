a84f7146a62769370e44a5ac16b4d42728dcfe24
Unmesh Patil <punmesh56@gmail.com>
2026-08-12 10:20:07 +0530

    Time: 63 ms (88.7%), Space: 114.4 MB (5.69%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
@@ -3,18 +3,18 @@ class Solution {
         Map<Integer, Integer> freq = new HashMap<>();
         int left = 0;
         int maxLen = 0;
-        
+
         for (int right = 0; right < nums.length; right++) {
             freq.merge(nums[right], 1, Integer::sum);
-            
+
             while (freq.get(nums[right]) > k) {
                 freq.merge(nums[left], -1, Integer::sum);
                 left++;
             }
-            
+
             maxLen = Math.max(maxLen, right - left + 1);
         }
-        
+
         return maxLen;
     }
 }

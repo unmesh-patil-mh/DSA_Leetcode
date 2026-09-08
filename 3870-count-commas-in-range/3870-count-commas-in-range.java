b433a50268547f3f32ed0ca64f5c3838b1242f1c
Unmesh Patil <punmesh56@gmail.com>
2026-09-08 12:15:17 +0530

    Time: 8 ms (8.47%), Space: 41.9 MB (99.42%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,24 +1,12 @@
 class Solution {
     public int countCommas(int n) {
-        int num = n;
-        int digits = 0;
+        int count = 0;
 
-        while(n > 0){
-            n = n / 10;
-            digits = digits + 1;
-        }
-
-        if(digits < 4){
-            return 0;
-        }
-
-        int commas = 0;
-        if(digits >= 4){
-            for(int i = 1000 ; i <= num ; i++){
-                commas += 1;
+        for(int i = 1 ; i <= n ; i++){
+            if( i >= 1000){
+                count++;
             }
         }
-
-        return commas;
+        return count;
     }
 }
